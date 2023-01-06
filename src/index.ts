6c3816146aa942a36370a99a02aed95b8cb22269
web-js-master <web-js-master@gmail.com>
2023-01-06 10:46:45 +0300

    refactor: index.ts package.json tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
 import { port } from "./utils/port"
 import * as http from "http"
 import {del, get, post, put} from "./routes";
-
-
+import { processingResponse } from "./utils/processingResponse";
 
 export const server = http.createServer((req, res) => {
-	switch (req.method) {
-		case 'GET': {
-			get(req, res)
-			break
-		}
-		case 'POST': {
-			post(req, res)
-			break
-		}
-		case 'PUT': {
-			put(req, res)
-			break
-		}
-		case 'DELETE': {
-			del(req, res)
-			break
-		}
-	}
+    try {
+        switch (req.method) {
+            case 'GET': {
+                get(req, res)
+                break
+            }
+            case 'POST': {
+                post(req, res)
+                break
+            }
+            case 'PUT': {
+                put(req, res)
+                break
+            }
+            case 'DELETE': {
+                del(req, res)
+                break
+            }
+            default:
+                throw new Error('Unsupported operation')
+        }
+    } catch (error) {
+		processingResponse(res, 500, {'message' : 'Server error'})
+    }
 })
 
 server.listen(port, () => {
-	console.log(`Server running on port: ${port}`)
+	console.log(`Server running on port: ${port}, whith pid: ${process.pid}`)
 })
 
 server.on('close', () => {
