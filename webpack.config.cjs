c3c9b0ee03fac0986ad7fd92ba0a4849bd06a8e6
web-js-master <web-js-master@gmail.com>
2023-01-13 21:49:44 +0300

    feat: eslint, jest, webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,21 @@
+import path from 'path'
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
